4618316f9a451e0029cb7ed38bd2c96372e27456
Peppozz <145689738+Peppozz@users.noreply.github.com>
2026-05-04 15:11:43 +0200

    Add files via upload
    struttura data
    
    file con struct data

---
+++ data_psd.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct typedef{
+	int anno;	
+	int mese;	
+	int giorno;	//se mese==1 || 3 || 5 || 7 || 8 || 10 || 12  giorno==3	\ se mese==4 || 6 || 9 || 11 giorni==30 \ se mese==2 28 o 29 se bisestile
+}data;
+
