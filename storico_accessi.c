af01f3a9fd327bbe1afeebd4d14de1dd1645ebbe
masdcsacsadf <massimo.zak.it@gmail.com>
2026-05-04 15:58:45 +0200

    Add files via upload
    struct storico_accessi
    
    Aggiunta del file contenete lo storico_accessi

---
+++ storico_accessi.c
@@ -0,0 +1,15 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include<string.h>
+#include<data_psd.h>
+#include<aula_studio.h>//prendiamo la variabile matricola
+#include<prenotazione.h>
+#include<noprenotati.h>
+#include<lista.h>
+#include<coda.h>
+
+
+struct typedef{
+    int matricolAcc;//matricola dello studente,nome differente per ridurre confusione
+    int tipoAcc;//come ha fatto l accesso l utente
+}storico_accessi
