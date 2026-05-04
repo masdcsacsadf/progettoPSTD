bc9cc34bd699a9f5ec7a84caa00d10d7339f8817
masdcsacsadf <massimo.zak.it@gmail.com>
2026-05-04 14:56:14 +0200

    Add files via upload
    struct studente
    
    Aggiunta delle variabili nella struct studente ,le  variabili sono matricola,nome,corso,stato.

--- studente.c
+++ studente.c
@@ -1,6 +1,11 @@
 #include<stdlib.h>
 #include<stdio.h>
+#include<string.h>
 
 struct typedef{
-
+    char nome[100];
+    int matricola;
+    char corso[100];// corso di laurea dello studente
+    int stato;
+/*variabile 'stato' server per controllare se lo studente :ha il posto =2 o ha prenotato=1 o sta aspettano nella coda di attessa =0 */
 }studente;
