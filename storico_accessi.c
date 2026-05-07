8efb923c3d04c5c8c666dbc75bd44f2907e336f8
masdcsacsadf <massimo.zak.it@gmail.com>
2026-05-07 09:06:52 +0200

    aggiunta funzioni
    "creaIst" e "stampaStorico",sono state aggiunte al file

--- storico_accessi.c
+++ storico_accessi.c
@@ -1,9 +1,18 @@
 #include<stdlib.h>
 #include<stdio.h>
 
-
-
 struct typedef{
     int matricolAcc;//matricola dello studente,nome differente per ridurre confusione
     int tipoAcc;//come ha fatto l accesso l utente
 }storico_accessi
+
+storico_accessi creaIst(int a,int b){
+    storico_accessi c=(storico_accessi)malloc(sizeof(struct storico_accessi));
+    c.matricolAcc=a;
+    c.tipoAcc=b;
+    return c;
+}
+void stampaStorico(storico_accessi a){
+    printf("matricola studente: %d",a.matricolAcc);
+    printf("tipo di accesso: %d",a.tipoAcc);
+}
