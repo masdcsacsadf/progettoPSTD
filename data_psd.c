84170a44588e600b47de6a1b9464e7e59c322704
masdcsacsadf <massimo.zak.it@gmail.com>
2026-05-09 13:32:39 +0200

    aggiunta nuova struct
    stata aggiunta la struct dataCorrente per future appilicazioni del progetto

--- data_psd.c
+++ data_psd.c
@@ -7,3 +7,7 @@ struct typedef{
 	int giorno;	//se mese==1 || 3 || 5 || 7 || 8 || 10 || 12  giorno==3	\ se mese==4 || 6 || 9 || 11 giorni==30 \ se mese==2 28 o 29 se bisestile
 }data;
 
+struct typedef{
+	data date;
+	int ora;
+}dataCorrente;
