0c72e82136811e400a0f24d2c95c9ebaa6973064
Peppozz <145689738+Peppozz@users.noreply.github.com>
2026-05-09 21:54:37 +0200

    Add files via upload
    correzione codice
    
    corretto il file (nello specifico la struct aveva una dichiarazione sbagliata)
    cambiato da "struct typedef" a "typedef struct"

--- data_psd.c
+++ data_psd.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-struct typedef{
+typedef struct{
 	int anno;	
 	int mese;	
 	int giorno;	//se mese==1 || 3 || 5 || 7 || 8 || 10 || 12  giorno==3	\ se mese==4 || 6 || 9 || 11 giorni==30 \ se mese==2 28 o 29 se bisestile
 }data;
 
-struct typedef{
+typedef struct{
 	data date;
 	int ora;
 }dataCorrente;
