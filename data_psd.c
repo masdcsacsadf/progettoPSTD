eaaa4f2cb8341170fbe4b491fa195428de4a0d40
masdcsacsadf <massimo.zak.it@gmail.com>
2026-05-09 13:52:56 +0200

    aggiunta impostaData
    stata aggiunta la funzione "impostaData" al file

--- data_psd.c
+++ data_psd.c
@@ -11,3 +11,38 @@ struct typedef{
 	data date;
 	int ora;
 }dataCorrente;
+
+data impostaData(int a,int m,int g){
+	data t;
+	t.anno=a;
+	if((m==1)||(m==3)||(m=5)||(m==7)||(m==8)||(m==10)||(m==12)){
+		if((g>0)&&(g<=31)){
+			t.mese=m;
+			t.giorno=g;
+			return t;
+		}
+	}
+	if((m==4)||(m==6)||(m==9)||(m==11)){
+		if((g>0)&&(g<=30)){
+			t.mese=m;
+			t.giorno=g;
+			return t;
+		}		
+	}
+	if(m==2){
+		if((a%4==0)&&(a%100!=0)||(a%400==0)){
+			if((g>0)&&(g<=29)){
+				t.mese=m;
+				t.giorno=g;
+				return t;
+			}	
+		}
+		if((g>0)&&(g<=28)){
+			t.mese=m;
+			t.giorno=g;
+			return t;
+		}	
+	}
+	t.anno=-1;
+	return t;
+}
