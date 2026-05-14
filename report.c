0b0a7abe1c2a0441804aba2bc1a78062116ab4c9
masdcsacsadf <massimo.zak.it@gmail.com>
2026-05-14 11:05:38 +0200

    creazione file report.c
    creazione del file "report.c" e al interno la struct "report"

---
+++ report.c
@@ -0,0 +1,11 @@
+#include<stdlib.h>
+#include<stdio.h>
+
+typedef struct{
+  int preTot
+  int accessoT
+  int noShow
+  int stuAttesa
+  int oraMin    //oraMin e oraMax serve per mostrare la fascio oraria occupata nella sala in media
+  int oraMax
+}report
