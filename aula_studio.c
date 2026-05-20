d0868926c29e3919cf2cf2dca9e99ca5f797f483
Peppozz <145689738+Peppozz@users.noreply.github.com>
2026-05-20 08:46:15 +0200

    aggiunta varibile oraMax
    stata agginta la varibile oraMax a riga 13,per poter sapere quando si libera il posto

--- aula_studio.c
+++ aula_studio.c
@@ -1,9 +1,16 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+/*questa struttura dati serve rappresentare i posti nel aula.
+al interno ha 2 variabili:la prima varibile prende la matricola dello studente
+e quando il posto e libero lo imèpone a -1
+la seconda variabile prende l ora di occupazione,cioe quando si libera il posto,
+per poter aggirnare i posti usado l aggiornamento automatico per orario
+*/
 
 struct typedef{
-	int status; //-1 se posto libero  numero matricola se occupato
+	int status;
+	int oraMax;
 }posti;
 
 
