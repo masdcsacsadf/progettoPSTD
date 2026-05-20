6b5da1fcb39e8e316ea7d960e70ddabf4f0afc6e
Peppozz <145689738+Peppozz@users.noreply.github.com>
2026-05-20 09:44:34 +0200

    aggiunta funzione getLibero
    aggiunta funzione getLibero per sapere se c'è un posto libero in aula studio

--- aula_studio.c
+++ aula_studio.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-/*questa struttura dati serve rappresentare i posti nel aula.
+/*
+questa struttura dati serve rappresentare i posti nel aula.
 al interno ha 2 variabili:la prima varibile prende la matricola dello studente
 e quando il posto e libero lo imèpone a -1
 la seconda variabile prende l ora di occupazione,cioe quando si libera il posto,
@@ -15,4 +16,18 @@ struct typedef{
 
 int getStatus(){
 	return status;
+}
+
+
+/*
+funzione che scorre l'array dei posti e ritorna -1 in caso il posto sia libero
+se l'array non ha nessun posto libero ritorna il valore -2
+*/
+int getLibero(posti p[]. int n){
+	for(int i=0;i<n; i++){
+		if(p[i].status==-1){
+			return -1;
+		}
+	}
+	return -2; 
 }
