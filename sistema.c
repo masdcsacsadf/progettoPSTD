1c59f21bfa48b71876299cda8809d9c73a800166
Peppozz <145689738+Peppozz@users.noreply.github.com>
2026-05-23 12:16:17 +0200

    Add files via upload
    Aggiunte funzioni di registrazione e parametri nella struct dati
    
    aggiunte le funzioni "regStudenti" per registrare gli studenti in una lista e "prenotazione" per prenotare un posto in aula studio.
    
    aggiunte le variabili "list studenti","bool flagLib" per vedere se un posto è libero o meno in aula studio e "int limitPren" per limitare il numero di richieste che si possono fare in modo tale da non intasare la lista dei prenotati

--- sistema.c
+++ sistema.c
@@ -6,11 +6,64 @@
 #include<aula_studio.h>
 #include<data.h>
 #include<report.h>
-#include <studente.h>
+#define LUNG 100
 /*lo scopo di questo file e quello di contenere tutte le funzioni per far funzionare
 il nostro progetto.il motivo di questa scelta e per rendere piu facile la 
 manipolazione del codice e il suo debbugging.
 FIRMATO da Maxim Zakharov*/
+
+void prenotazione(dati*t){    
+    
+    if(t->limitPren >= 50){
+        printf("ERRORE: SPAZIO INSUFFICIENTE PER L'OPERAZIONE");
+        return;
+    }
+    
+    data temp;
+    prenotazioni x;
+    int matricola, mese, anno, giorno, minOra, maxOra;
+
+    scanf("%d",&matricola);
+    setMatr(x, matricola);
+
+    do{
+        scanf("%d",&mese);
+        scanf("%d",&anno);
+        scanf("%d",&giorno);
+        temp=impostaData();
+
+        scanf("%d",&minOra);
+        scanf("%d",&maxOra);
+
+    }while((dataValidCheck(minOra,temp,t->time) == 1) && (minOra < 0) || (maxOra > 23) || (minOra >= maxOra))
+    
+    setMinO(x, minOra);
+    setMaxO(x, maxOra);
+    setData(x, temp);
+
+    list consList( x, t->prenotazioni); 
+
+    t->limitPren ++;
+}
+
+
+void regStudenti(dati*t){
+
+    studenti x;
+    int matricola;
+    char nome[],corso[];
+
+    scanf("%d",&matricola);
+    setMatr(x, matricola);
+
+    scanf("%c",&nome);
+    setNome(x, nome);
+    
+    scanf("%c",&corso);
+    setCorso(x, corso);
+
+    consList(x,t->studenti);
+}
 
 /*questa e una struct che contiene dentro di se tutte le strutture dati del sistema
  e variabili di supporto per far lavorare le varie funzioni che si troverano al 
@@ -22,10 +75,10 @@ typedef struct{
     list prenotazioni;
     list noprenotati;
     list storico_accessi;
-    posti sala[100];
+    posti sala[LUNG];
     dataCorrente time;
     report rapporto;
     list studenti;
-    int flagLib = 0;
+    bool flagLib = 0;
     int limitPren = 0;
 }dati;
