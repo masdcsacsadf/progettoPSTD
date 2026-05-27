5b2fd1852e88d5e854c09b2dcc94f82baf2c8ac1
masdcsacsadf <massimo.zak.it@gmail.com>
2026-05-27 16:19:56 +0200

    aggiunte funzioni set
    sono state aggiunte le funzioni set delle varie varibili della struttura dati;

--- report.c
+++ report.c
@@ -8,4 +8,29 @@ typedef struct{
   int stuAttesa
   int oraMin    //oraMin e oraMax serve per mostrare la fascio oraria occupata nella sala in media
   int oraMax
-}report
+}report;
+
+report setPreTot(report t,int n){
+  t.preTot=n;
+  return t;
+}
+report seTaccessoT(report t,int n){
+  t.accessoT=n;
+  return t;
+}
+report setNoshow(report t,int n){
+  t.noShow=n;
+  return t;
+}
+report setStuAttessa(report t,int n){
+  t.stuAttessa=n;
+  return t;
+}
+report setOraMin(report t,int n){
+  t.oraMin=n;
+  return t;
+}
+report setOraMax(report t,int n){
+  t.oraMax=n;
+  return t;
+}
