be9187c798bd31acfd0c5d910b47388c0f7f5dd6
Peppozz <145689738+Peppozz@users.noreply.github.com>
2026-06-02 12:09:24 +0200

    aggiunta funzione "stampa"
    aggiunta funzione di "stampa" e aggiustati alcuni errori di battitura nel codice

--- report.c
+++ report.c
@@ -2,19 +2,19 @@
 #include<stdio.h>
 
 typedef struct{
-  int preTot
-  int accessoT
-  int noShow
-  int stuAttesa
-  int oraMin    //oraMin e oraMax serve per mostrare la fascio oraria occupata nella sala in media
-  int oraMax
+  int preTot;
+  int accessoT;
+  int noShow;
+  int stuAttesa;
+  int oraMin;    //oraMin e oraMax serve per mostrare la fascio oraria occupata nella sala in media
+  int oraMax;
 }report;
 
 report setPreTot(report t,int n){
   t.preTot=t.preTot+n;
   return t;
 }
-report seTaccessoT(report t,int n){
+report setAccessoT(report t,int n){
   t.accessoT=t.accessoT+n;
   return t;
 }
@@ -23,7 +23,7 @@ report setNoshow(report t,int n){
   return t;
 }
 report setStuAttessa(report t,int n){
-  t.stuAttessa=stuAttessa+n;
+  t.stuAttesa=t.stuAttesa+n;
   return t;
 }
 report setOraMin(report t,int n){
@@ -33,4 +33,8 @@ report setOraMin(report t,int n){
 report setOraMax(report t,int n){
   t.oraMax=n;
   return t;
+}
+
+void stampa(report t){
+  printf("prenotazione totali=%d\n accessi totali= %d\n studenti assentati= %d\n studenti in attesa= %d\n ora minima= %d\n ora massima= %d", &t.preTot, &t.accessoT, &t.noShow, &t.stuAttesa, &t.oraMin, &t.oraMax);
 }
