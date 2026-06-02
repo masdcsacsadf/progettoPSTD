08c52b90f8254c243f6da4299bd6368a2d842611
Peppozz <145689738+Peppozz@users.noreply.github.com>
2026-06-02 11:42:09 +0200

    aggiunto file ".h"
    aggiunto file ".h" del file "report.c"

---
+++ report.h
@@ -0,0 +1,13 @@
+typedef struct report * t;
+
+report setPreTot(report t,int n);
+
+report seTaccessoT(report t,int n);
+
+report setNoshow(report t,int n);
+
+report setStuAttessa(report t,int n);
+
+report setOraMin(report t,int n);
+
+report setOraMax(report t,int n);
